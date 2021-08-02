35f5be9318ae3fdc6ef63e188c2d752f3ec7a671
PedroAugustoCR <pedroaugusto_cr@hotmail.com>
2021-08-02 18:01:41 -0300

    introduction forms in html and component

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
 
+  valor: number;
+  destino: number;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  transferir() {
+    console.log('Solicitada nova transfêrencia');
+    console.log(this.valor);
+    console.log(this.destino);
+  }
 }
